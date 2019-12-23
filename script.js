836c865485d493049fa9f8eba9de7ad4e4e8a149
Miron Gertis <56720084+datennutzung@users.noreply.github.com>
2019-12-23 13:42:43 +0100

    Happy Christmas Tree

--- script.js
+++ script.js
@@ -1,40 +1,36 @@
-var elem = {
-    width: document.getElementById("width"),
-    height: document.getElementById("height"),
-    stem_height: document.getElementById("stem_height"),
-    stem_width: document.getElementById("stem_width"),
-};
-
-function generate() {
+function generate(num = 0) {
     let tree_char = '*';
-    for (const key in elem) 
-        if (!elem[key].checkValidity())
-            return false;
-
-    let width = parseInt(elem.width.value);
-    let height = parseInt(elem.height.value);
-    let stem_height = parseInt(elem.stem_height.value);
-    let stem_width = parseInt(elem.stem_width.value);
-
-    let current_layer_width = 1;
+    let maxHeight = num;
+    if (num == 0)
+        maxHeight = getRandomInt(8, 23);
+    document.getElementById("height_label").innerText = maxHeight;
+    document.getElementById("treesize_range").value = maxHeight;
     let tree_html = "";
-    for (let i = 1; i < height+1; i++) {
-        for (let o = 0; o < current_layer_width; o++) {
+    let nextWidth = 0;
+    let layerHeight = 6;
+    let lastLayer = 0;
+    for (let height = 0; height < maxHeight; height++) {
+        nextWidth++;
+        lastLayer++;
+        if (layerTree(maxHeight-height, layerHeight, lastLayer)) {
+            nextWidth -=3;
+            lastLayer = 0;
+        }
+        for (let width = 0; width < nextWidth; width++) {
             tree_html += tree_char;
         }
-        let imod = i%width;
-        console.log(imod)
-        current_layer_width = imod+1
-        
         tree_html += "<br>";  
     }
 
-    for (let i = 0; i < stem_height; i++) {
-        for (let o = 0; o < stem_width; o++) {
-            tree_html += tree_char;
-        }
-        tree_html +="<br>";
-    }
-
+    tree_html += "| |";
     document.getElementById("tree").innerHTML = tree_html;
+}
+
+/** A function that gives you a random integer in [min, max) */
+function getRandomInt(min, max) {
+    return Math.floor(min+Math.random()*(max-min));
+}
+
+function layerTree(remainingHeight, layerHeight, lastLayer) {
+    return (layerHeight == lastLayer && remainingHeight >= layerHeight);
 }
